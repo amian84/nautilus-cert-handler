b2fc0c73dbd2347183b9ca931cf6350daf198492
David Amian <damian@emergya.com>
2011-09-08 12:11:53 +0200

    Create directory structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from DistUtilsExtra.command import *
+
+setup(name='nautilus-cert-handler',
+ version='0.1',
+    author='David Amian',
+    author_email='damian@emergya.com',
+    data_files=[]
+    cmdclass = { "build" : build_extra.build_extra,
+        "build_i18n" :  build_i18n.build_i18n,
+        "clean": clean_i18n.clean_i18n, 
+        }
+    )
