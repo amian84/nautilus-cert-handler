97724ccc219b29f5e0a6457607553b20069ee144
David Amian <damian@emergya.com>
2011-09-12 12:22:46 +0200

    Fix bug in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='nautilus-cert-handler',
     author='David Amian',
     author_email='damian@emergya.com',
     data_files=[('share/nautilus-python/extensions',['scripts/nautilus-cert-handler.py']),
-                ('/var/lib/update-notifier/user.d',['data/nautilus-cert-handler-notification'])]
+                ('/var/lib/update-notifier/user.d',['data/nautilus-cert-handler-notification'])],
     cmdclass = { "build" : build_extra.build_extra,
         "build_i18n" :  build_i18n.build_i18n,
         "clean": clean_i18n.clean_i18n, 
